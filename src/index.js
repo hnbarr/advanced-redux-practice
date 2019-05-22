2e773f0be3fb12971610e5ed562eb0fe4837fd8d
Heather Barr <hnbarr@yahoo.com>
2019-05-21 20:36:34 -0500

    removed state from indexjs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import state from './state';
 import { Provider } from 'react-redux'
 import store from './store'
 
